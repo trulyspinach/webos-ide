36a63e229b26050b5297cda765de067ddd4c651f
Allen Hui <allenhui1220@outlook.com>
2019-04-28 01:05:50 +1000

    Should be done

--- server/SPINACH_rope_struct/test.py
+++ server/SPINACH_rope_struct/test.py
@@ -2,12 +2,28 @@ import spinach_rope as r
 
 #
 a = r.rope("hello,")
-b = r.rope(" world")
-c = r.rope(" 你好世界！")
+b = r.rope("world")
+c = r.rope(" i am allen")
 
 # print(r.strconcat("de","dejujijijijij"))
 # print(a.get_str() + b.get_str())
 
 d = r.concat(a, b)
-print(d.get_str())
-print(r.concat(d, c).get_str())
+e = r.concat(d, c)
+# for i in range(20):
+#     print(e.indexat(i))
+print(e.get_str())
+
+right = r.split(e, 10)
+print(right.get_str())
+print(e.get_str())
+
+e = r.concat(e, right)
+print(e.get_str())
+
+e.insert(2, "continue last dying breath")
+print(e.get_str())
+e.delete(2, 3)
+print(e.get_str())
+
+
